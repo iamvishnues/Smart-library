47b079fe2a0db808e668b49a3b22a11997c81db4
Vishnu ES <55495318+Vishnu-es@users.noreply.github.com>
2020-07-24 20:13:43 +0530

    Add files via upload

---
+++ src/javaconnect.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vishnu ES
+ */
+import java.sql.*;
+
+import javax.swing.JOptionPane;
+public class javaconnect {
+    Connection conn;
+    public static Connection ConnecrDb(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/library?zeroDateTimeBehavior=convertToNull","root","");
+            return conn;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
